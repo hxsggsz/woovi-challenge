cfdda83d6aae4827feaf8ddeaf2438ef40fdfede
hxsggsz <vt.hugo.2021@gmail.com>
2024-07-10 12:52:11 -0300

    feat: setup material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 import { useTranslation } from "react-i18next";
+import { useThemes } from "./context/themeContext";
+import { Box, Container, Typography } from "@mui/material";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const { toggleTheme } = useThemes();
+
   const { t } = useTranslation();
 
   return (
-    <>
+    <Box>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -19,17 +22,18 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <Typography>Vite + React</Typography>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button onClick={toggleTheme}>togle theme</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">{t("title:main")}</p>
-    </>
+    </Box>
   );
 }
 
